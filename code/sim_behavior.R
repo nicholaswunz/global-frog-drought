f8b074154e5d8de3f47a9d780628c9443adaa836
Urtzi_EU <urtzi.enriquez@gmail.com>
2022-11-30 04:03:23 +0100

    remove unnecessary files, rename, and fix path

--- code/sim_behavior.R
+++ code/sim_behavior.R
@@ -4,7 +4,7 @@ setwd('~/Dropbox/1_papers/collaborations/Niky Wu/global-frog-drought/code/')
 rm(list=ls())
 
 library(NicheMapR)
-source('micro_global_drought2.R')
+source('micro_global_drought.R')
 source('behav_functions.R')
 
 longlat    <- c(153.09249, -27.6235) # Karawatha, QLD.
