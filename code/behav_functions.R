b4abc846708e5d20842e8e99f6e8e8984872243c
Urtzi_EU <urtzi.enriquez@gmail.com>
2022-11-30 03:51:15 +0100

    add functions

--- code/behav_functions.R
+++ code/behav_functions.R
@@ -1,8 +1,41 @@
 
+# function to retrieve output from micro objects
+retrieve.output <- function(micro){
+  
+  micro.output <- list()
+  
+  # retrieve ouptut
+  # date <- weather$date[1:nrow(micro$metout)]
+  metout <- as.data.frame(micro$metout) # retrieve above ground microclimatic conditions, min shade
+  shadmet <- as.data.frame(micro$shadmet) # retrieve above ground microclimatic conditions, max shade
+  soil <- as.data.frame(micro$soil) # retrieve soil temperatures, minimum shade
+  shadsoil <- as.data.frame(micro$shadsoil) # retrieve soil temperatures, maximum shade
+  soilmoist <- as.data.frame(micro$soilmoist) # retrieve soil moisture, minimum shade
+  shadmoist <- as.data.frame(micro$shadmoist) # retrieve soil moisture, maximum shade
+  humid <- as.data.frame(micro$humid) # retrieve soil humidity, minimum shade
+  shadhumid <- as.data.frame(micro$shadhumid) # retrieve soil humidity, maximum shade
+  soilpot <- as.data.frame(micro$soilpot) # retrieve soil water potential, minimum shade
+  shadpot <- as.data.frame(micro$shadpot) # retrieve soil water potential, maximum shade
+  
+  # append dates
+  micro.output[['metout']] <- metout
+  micro.output[['shadmet']] <- shadmet
+  micro.output[['soil']] <- soil
+  micro.output[['shadsoil']] <- shadsoil
+  micro.output[['soilmoist']] <- soilmoist
+  micro.output[['shadmoist']] <- shadmoist
+  micro.output[['humid']] <- humid
+  micro.output[['shadhumid']] <- shadhumid
+  micro.output[['soilpot']] <- soilpot
+  micro.output[['shadpot']] <- shadpot
+  
+  return(micro.output)
+}
+
 
 
 # function to define when an animal will be active
-activity <- function(behav=c('diurnal', 'nocturnal'), Tmax=30, Tmin=10, Z=Z){
+activity <- function(behav=c('diurnal', 'nocturnal', 'both'), Tmax=30, Tmin=10, Z=Z){
   if(behav == 'diurnal' & Z == 90 |
      behav == 'nocturnal' & Z < 90){
     FALSE
@@ -12,73 +45,81 @@ activity <- function(behav=c('diurnal', 'nocturnal'), Tmax=30, Tmin=10, Z=Z){
 }
 
 
+# function to find frogs position below-ground
+seldep <- function(){
+  
+}
+
+
 # set the environment given activity (1 = active above-ground; 0 = below-ground)
-environment <- function(activity){
+environment <- function(micro.output, activity, x){
+  
   if(activity){
     # above-ground
-    TA <- TAs[x]
-    TGRD <- TGRDs[x]
-    TSKY <- TSKYs[x]
-    VEL <- VELs[x]
-    RH <- RHs[x]
-    QSOLR <- QSOLRs[x]
+    metout <- micro.output$metout
+    soil <- micro.output$soil
+    
+    # get required inputs
+    TA <- metout$TALOC[x]
+    TGRD <- soil$D0cm[x]
+    TSKY <- metout$TSKYC[x]
+    VEL <- metout$VLOC[x]
+    RH <- metout$RHLOC[x]
+    QSOLR <- metout$SOLR[x]
   }
   else {
     # below-ground
-    TA <- TAs[x]
-    TGRD <- TA
-    TSKY <- TA
-    VEL <- 0.01
-    RH <- 99
-    QSOLR <- 0.0
-    # EMISSB <- 1.0
-    # EMISSK <- 1.0
+
+    # TA <- 10
+    # TGRD <- TA
+    # TSKY <- TA
+    # VEL <- 0.01
+    # RH <- 99
+    # QSOLR <- 0.0
+    # # EMISSB <- 1.0
+    # # EMISSK <- 1.0
+    # 
+    
+    seldep()
   }
   
-  return(TA=TA, TGRD=TGRD, TSKY=TSKY, VEL=VEL, RH=RH, QSOLR=QSOLR)
+  return(list(TA=TA, TGRD=TGRD, TSKY=TSKY, VEL=VEL, RH=RH, QSOLR=QSOLR))
 }
 
 
 
 
 # function to run ectotherm simulations
-sim.ecto <- function(micro){
+sim.ecto <- function(micro, behav='diurnal', Tmax=30, Tmin=10){
   
-  # extract full sun conditions
-  metout <- as.data.frame(micro$metout)
-  soil <- as.data.frame(micro$soil)
+  micro.output <- retrieve.output(micro)
   
-  # get required inputs
-  TAs <- metout$TALOC
-  TGRDs <- soil$D0cm
-  TSKYs <- metout$TSKYC
-  VELs <- metout$VLOC
-  RHs <- metout$RHLOC
-  QSOLRs <- metout$SOLR
-  Zs <- metout$ZEN
-  
-  lapply(1:(micro$ndays * 24), function(x){
-    act <- activity(behav='diurnal', Tmax=30, Tmin=10, Z=Zs[x])
+  lapply(1:nrow(micro.output$metout), function(x){
+    
+    zenith <- micro.output$metout$ZEN[x]
+    act <- activity(behav=behav, Tmax=Tmax, Tmin=Tmin, Z=zenith)
+    env <- environment(micro.output, act, x)
     
     ecto <- ectoR_devel(Ww_g = 40,
-                        shape = 3,
+                        shape = 4,
                         alpha = 0.85,
                         M_1 = 0,
                         postur = 0,
                         pantmax = 0,
                         pct_cond = 40,
+                        pct_wet = 80,
                         K_sub = 0.1,
                         alpha_sub = (1 - micro$REF),
                         elev = micro$elev,
-                        TA = TAs[x],
-                        TGRD = TGRDs[x],
-                        TSUBST = TGRDs[x],
-                        TSKY = TSKYs[x],
-                        VEL = VELs[x],
-                        RH = RHs[x],
-                        QSOLR = QSOLRs[x],
-                        Z = Zs[x])
-    
+                        TA = env$TA,
+                        TGRD = env$TGRD,
+                        TSUBST = env$TGRD,
+                        TSKY = env$TSKY,
+                        VEL = env$VEL,
+                        RH = env$RH,
+                        QSOLR = env$QSOLR,
+                        Z = zenith)
+    ecto$TC
   })
 }
 
--- code/sim_behavior.R
+++ code/sim_behavior.R
@@ -1,14 +1,17 @@
 
-setwd('~/Dropbox/1_papers/collaborations/Niky Wu/code')
+setwd('~/Dropbox/1_papers/collaborations/Niky Wu/global-frog-drought/code/')
 
 rm(list=ls())
 
+library(NicheMapR)
 source('micro_global_drought2.R')
 source('behav_functions.R')
 
-micro <- micro_global_drought(rainfact = 0.54)
+longlat    <- c(153.09249, -27.6235) # Karawatha, QLD.
+micro <- micro_global_drought(loc=longlat, rainfact = 0.54,
+                              runmoist = T, runshade = T, timeinterval = 12)
 
-sim.res <- sim.ecto(micro)
+sim.res <- sim.ecto(micro, behav = 'both')
 
 sim.res <- unlist(sim.res)
 plot(sim.res, type='l')
